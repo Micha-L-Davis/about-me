7cee11e7c68b34bb2fddf05750fc10159f8d0f72
Micha Davis <m.leigh.davis@gmail.com>
2022-01-11 16:09:38 -0800

    replaced console logs with alert calls

--- js/app.js
+++ js/app.js
@@ -12,58 +12,73 @@ let answer;
 
 answer = prompt('Micha Pop Quiz!\nQ1: Was Micha once a sailor?').toLowerCase();
 if (answer === 'y' || answer === 'yes'){
-  console.log(buzzText);
+  // console.log(buzzText);
+  alert(buzzText);
 }
 else if (answer === 'n' || answer === 'no'){
-  console.log(dingText);
+  // console.log(dingText);
+  alert(dingText);
 }
 else {
-  console.log(errText);
+  // console.log(errText);
+  alert(errText);
 }
 
 answer = prompt('Micha Pop Quiz!\nQ2: Does Micha have a daughter?').toLowerCase();
 if (answer === 'n' || answer === 'no'){
-  console.log(buzzText);
+  // console.log(buzzText);
+  alert(buzzText);
 }
 else if (answer === 'y' || answer === 'yes'){
-  console.log(dingText);
+  // console.log(dingText);
+  alert(dingText);
 }
 else {
-  console.log(errText);
+  // console.log(errText);
+  alert(errText);
 }
 
 answer = prompt('Micha Pop Quiz!\nQ3: Did Micha attend college in Maryland?').toLowerCase();
 if (answer === 'y' || answer === 'yes'){
-  console.log(buzzText);
+  // console.log(buzzText);
+  alert(buzzText);
 }
 else if (answer === 'n' || answer === 'no'){
-  console.log(dingText);
+  // console.log(dingText);
+  alert(dingText);
 }
 else {
-  console.log(errText);
+  // console.log(errText);
+  alert(errText);
 }
 
 answer = prompt('Micha Pop Quiz!\nQ4: Has Micha ever released a software product before?').toLowerCase();
 if (answer === 'n' || answer === 'no'){
-  console.log(buzzText);
+  // console.log(buzzText);
+  alert(buzzText);
 }
 else if (answer === 'y' || answer === 'yes'){
-  console.log(dingText);
+  // console.log(dingText);
+  alert(dingText);
 }
 else {
-  console.log(errText);
+  // console.log(errText);
+  alert(errText);
 }
 
 answer = prompt('Micha Pop Quiz!\nQ5: Does Micha keep any pets?').toLowerCase();
 if (answer === 'n' || answer === 'no'){
-  console.log(buzzText);
+  // console.log(buzzText);
+  alert(buzzText);
 }
 else if (answer === 'y' || answer === 'yes'){
-  console.log(dingText);
+  // console.log(dingText);
+  alert(dingText);
 }
 else {
-  console.log(errText);
+  // console.log(errText);
+  alert(errText);
 }
 
 alert(`Well that sure was fun, wasn't it, ${user}? I thought so, too. Stick around once these dialogue boxes go away, and you can read all about me. And... uh. Not sure how to end this. Never hosted a gameshow before... uh...
-  \nThanks for playing, and please remember to spay and neuter your pets!`);
+\nThanks for playing, and please remember to spay and neuter your pets!`);
