f1be221a48ee223d1d4af510f2885b0ed1d93dfb
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-01-13 13:23:04 -0800

    Functionerized Yes/No question

--- js/app.js
+++ js/app.js
@@ -18,35 +18,41 @@ let user = prompt('Hi there, friend! What\'s your name?');
 alert(`Nice to meet you, ${user}! I'm Micha. Welcome to the "About Me" game! It's a very cool game where you can guess whether facts about me are accurate or not. To play, answer with either "yes/no" or "y/n" in the space provided.\nOkay?`);
 
 // Yes or No questions
-for (let i = 0; i < yesNoQuestions.length; i++){
-  answer = prompt(yesNoQuestions[i]).toLowerCase();
-  console.log(answer);
-  if (answer === 'y' || answer === 'yes'){
-    if (isYesAnswer[i] === false){
-      alert(buzzText);
-      continue;
+
+yesNoQuestion();
+
+function yesNoQuestion(){
+  for (let i = 0; i < yesNoQuestions.length; i++){
+    answer = prompt(yesNoQuestions[i]).toLowerCase();
+    console.log(answer);
+    if (answer === 'y' || answer === 'yes'){
+      if (isYesAnswer[i] === false){
+        alert(buzzText);
+        continue;
+      }
+      if (isYesAnswer[i] === true){
+        alert(dingText);
+        score++;
+        continue;
+      }
     }
-    if (isYesAnswer[i] === true){
-      alert(dingText);
-      score++;
-      continue;
+    else if (answer === 'n' || answer === 'no'){
+      if (isYesAnswer[i] === false){
+        alert(dingText);
+        score++;
+        continue;
+      }
+      if (isYesAnswer[i] === true){
+        alert(buzzText);
+        continue;
+      }
     }
-  }
-  else if (answer === 'n' || answer === 'no'){
-    if (isYesAnswer[i] === false){
-      alert(dingText);
-      score++;
-      continue;
+    else {
+      alert(errText);
     }
-    if (isYesAnswer[i] === true){
-      alert(buzzText);
-      continue;
-    }
-  }
-  else {
-    alert(errText);
   }
 }
+
 
 // Q6 guessing question
 correctGuess = false;
